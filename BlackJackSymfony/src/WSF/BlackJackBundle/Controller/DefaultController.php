bfb220600a75e36c9841a194ea2409d8596c24d7
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2014-10-22 21:28:51 +0200

    Added creation player

--- BlackJackSymfony/src/WSF/BlackJackBundle/Controller/DefaultController.php
+++ BlackJackSymfony/src/WSF/BlackJackBundle/Controller/DefaultController.php
@@ -3,11 +3,35 @@
 namespace WSF\BlackJackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use WSF\BlackJackBundle\Entity\Player;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
-        return $this->render('WSFBlackJackBundle:Default:index.html.twig', array('name' => $name));
+        $player = new Player();
+        $player->setName('Player name');
+        $player->setWallet('10000');
+
+        $form = $this->createFormBuilder($player)
+            ->add('name', 'text')
+            ->add('save', 'submit', array('label' => 'Play'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($player);
+            $em->flush();
+            return new Response('Created $player name '.$player->getName());
+        }
+
+        return $this->render('WSFBlackJackBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
